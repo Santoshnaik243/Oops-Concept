1224ee232afd26c4763c7084cd694e5c856e7b8a
SantoshOfficial243 <141751888+SantoshOfficial243@users.noreply.github.com>
2025-02-07 15:14:51 +0530

    Adding Annotation to Abstraction

--- Abstraction.java
+++ Abstraction.java
@@ -1,17 +1,25 @@
+/*Abstractin in java is process of hiding the implementation details and showing only the 
+essential features of an object. It allows developers to focus on what an object does 
+rather than how it does it.
+* Abstraction is achieved through abstract and interfaces.
+*/
+
 abstract class Animal{
     public void Legs(){
         System.out.println("All Animals have 4 legs:");
-
+        
     }
     abstract void Sound();
     abstract void Eat();
 
 }
 class Dog extends Animal{
+    @Override
     void Sound(){
         System.out.println("Bow.....");
 
     }
+    @Override
     void Eat(){
         System.out.println("meat........");
 
@@ -34,6 +42,7 @@ public class Abstraction {
         d1.Legs();
         d1.Sound();
         d1.Eat();
+
         c1.Legs();
         c1.Sound();
         c1.Eat();
